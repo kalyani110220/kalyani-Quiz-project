36687aa78962951c700c5539a58591258c7ae9cc
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-01 16:40:58 +0100

    app uses cors

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,10 @@
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
 
 const app: Express = express();
 const port: number = 3000;
+
+app.use(cors());
 
 app.get('/api', (req: Request, res: Response) => {
   res.json({ message: 'Quiz API running' });
