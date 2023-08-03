1682adb83b25cba3cd4652aa51a7365cf5c12e49
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-03 22:13:31 +0100

    success message included in post stat endpoint

--- server/services/post_information.ts
+++ server/services/post_information.ts
@@ -3,6 +3,6 @@ import { QueryParams, isNumberString } from "../helpers/check";
 
 export async function postStart (quizId: QueryParams) {
   if (!isNumberString(quizId)) throw new Error('Must contain quizId and it must be a number');
-  const roundInfo = await queryDatabase(`INSERT INTO round (quiz_id, answered, correct, status) VALUES (${quizId}, 0, 0, 'started') RETURNING id`);
+  const roundInfo = await queryDatabase(`INSERT INTO round (quiz_id, answered, correct, status) VALUES (${quizId}, 0, 0, 'started') RETURNING id, 'success' as message`);
   return roundInfo;
 }
